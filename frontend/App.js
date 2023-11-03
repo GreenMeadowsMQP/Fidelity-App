04233c051a607731165e538ff61523c722e69c4f
Emre Sabaz <emresbz@gmail.com>
2023-11-03 03:41:27 -0400

    New UI/Assets/Buttons w/o function

--- frontend/App.js
+++ frontend/App.js
@@ -1,57 +1,121 @@
 import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
 import axios from 'axios';
-import { ScrollView } from 'react-native';
+import Toolbar from './Toolbar.js'; 
+
+
+
+
+
 export default function App() {
-  const [data, setData] = useState([]);
+  
+  const [firstItem, setFirstItem] = useState(null);
+ 
 
-  useEffect(() => {
-    async function fetchNews() {
-        try {
-            const response = await axios.get('http://130.215.213.15:3000/getNews');// dont forget to change ip every time network errors 
-            console.log("API Response:", response.data);
-            if (response.data.content && Array.isArray(response.data.content)) {
-                setData(response.data.content);
-            }
-        } catch (error) {
-            console.error('Error fetching news:', error);
-        }
-    }
-    fetchNews();
-}, []);
+  // useEffect(() => {
+  //   async function fetchNews() {
+  //     try {
+  //       const response = await axios.get('http://192.168.1.12:3000/getNews'); // Make sure the IP is correct and if using on phone look up the IP on the expo app 
+  //       const newsContent = response.data.content;
+  //       if (newsContent && Array.isArray(newsContent) && newsContent.length > 0) {
+  //         setFirstItem(newsContent[0]);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching news:', error);
+  //     }
+  //   }
+  //   fetchNews();
+  // }, []);
+  const handleButton1Press = () => {
+    console.log('Button 1 pressed');
+    // Handle Button 1 Press
+  };
 
-return (
-    <ScrollView style={styles.container}>
-    {data.map((item, index) => (
-       <View key={index} style={styles.newsItem}>
-          <Text style={styles.symbol}>{item.symbol}</Text>
-          <Text style={styles.headline}>{item.headline}</Text>
-       </View>
-    ))}
- </ScrollView>
+  const handleButton2Press = () => {
+    console.log('Button 2 pressed');
+    // Handle Button 2 Press
+  };
+
+  const handleButton3Press = () => {
+    console.log('Button 3 pressed');
+    // Handle Button 3 Press
+  };
+
+  // If there is no data yet placeholder 
+  if (!firstItem) {
+    return (
+      <View style={styles.container}>
+         <View style={styles.header}>
+        <Image source={require('./assets/images/CompanyLogo.png')} style={styles.logo} />
+        <Text style={styles.name}>StockADE</Text> 
+      </View>
+        <View style={styles.newsItem}>
+        <Text>Loading...</Text>
+        </View>
+        <Toolbar />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>StockADE</Text> 
+        <Image source={require('./assets/images/CompanyLogo.png')} style={styles.logo} />
+      <View style={styles.newsItem}>
+        <Text style={styles.symbol}>{firstItem.symbol}</Text>
+        <Text style={styles.headline}>{firstItem.headline}</Text>
+      </View>
+      <Toolbar />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 16, // top green bar
-    paddingBottom: 16, // bottom green bar
-    paddingHorizontal: 16,
-    backgroundColor: "#FFFFFF", // White background
+    justifyContent: 'center', // Aligns children vertically in the middle
+    alignItems: 'center', // Aligns children horizontally in the middle
+    backgroundColor: '#F2E8CF', // White background
+    paddingTop:5,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center', // This will align items vertically in the center.
+    alignSelf: 'flex-start', // Aligns the header view to the start of the cross axis (left side).
+    padding: 0, // Adjust padding as needed.
   },
   newsItem: {
-    padding: 12,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
+    width: '100%', // Takes up 80% of the container width
+    height:'70%',
+    padding: 20, // Inner spacing for the news item
+    marginBottom:5, // Outer spacing for the news item
+    backgroundColor: '#A7C957', // Changed color for the news item to be visible
+    borderRadius: 10, // Rounded corners
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, // Elevation for Android (shadow for iOS is handled above)
   },
   symbol: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 8, // Space between the symbol and headline
   },
   headline: {
-    fontSize: 14,
-    marginTop: 4,
+    fontSize: 16,
   },
+  logo: {
+    width: 70, // Set the width as needed.
+    height: 70, // Set the height as needed.
+    resizeMode: 'contain', // Keeps the aspect ratio of the image.
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 30, 
+    marginLeft:-15,
+    
+  }
+
 });
---
+++ frontend/Toolbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Toolbar = ({ onButton1Press, onButton2Press, onButton3Press }) => {
+  // Define your button and overlay images
+  const buttonImages = {
+    button1: require('./assets/images/ToolbarImages/Vector.png'),
+    button2: require('./assets/images/ToolbarImages/Vector1.png'),
+    button3: require('./assets/images/ToolbarImages/Vector2.png'),
+    overlay1: require('./assets/images/ToolbarImages/Ellipse1.png'),
+    overlay2: require('./assets/images/ToolbarImages/Ellipse3.png'),
+    overlay3: require('./assets/images/ToolbarImages/Ellipse2.png'),
+  };
+
+  return (
+    <View style={styles.toolbar}>
+      {/* Button 1 */}
+      <TouchableOpacity style={styles.button} onPress={onButton1Press}>
+        <Image source={buttonImages.button1} style={styles.buttonBackground} />
+        <Image source={buttonImages.overlay1} style={styles.buttonOverlay} />
+      </TouchableOpacity>
+
+      {/* Button 2 */}
+      <TouchableOpacity style={styles.button} onPress={onButton2Press}>
+        <Image source={buttonImages.button2} style={styles.buttonBackground} />
+        <Image source={buttonImages.overlay2} style={styles.buttonOverlay} />
+      </TouchableOpacity>
+
+      {/* Button 3 */}
+      <TouchableOpacity style={styles.button} onPress={onButton3Press}>
+        <Image source={buttonImages.button3} style={styles.buttonBackground} />
+        <Image source={buttonImages.overlay3} style={styles.buttonOverlay} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    toolbar: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    button: {
+      // Set the dimensions large enough to fit the background and overlay.
+      width: 60, // Width of the button; you might not need to change this.
+      height: 60, // Height must be large enough to accommodate the overlay.
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 30, // This adds space between the buttons.
+      // If the overlay is still cut off, you might need to increase the height even more.
+    },
+    buttonBackground: {
+      position: 'absolute',
+      width: '100%', // Full width of the button.
+      height: '100%', // Full height of the button.
+      borderRadius: 30, // Half of the width to make it circular, adjust if needed.
+    },
+    buttonOverlay: {
+      position: 'absolute',
+      width: 80, // Width of the overlay in pixels.
+      height: 80, // Height of the overlay in pixels.
+      resizeMode: 'contain', // 'cover' will fill the area and might cut off parts, 'contain' will make sure all parts are visible.
+      borderRadius: 32, // Half of the overlay size to make it circular.
+      // Remove top and left percentages. Instead, center using the following technique:
+      alignSelf: 'center',
+      // Negative margins will effectively expand the touchable area and allow the overlay to be fully visible.
+      marginVertical: -(64 - 60) / 2, // Adjust this value based on your overlay size.
+    },
+  });
+
+export default Toolbar;
