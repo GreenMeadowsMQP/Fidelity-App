503e84f57de74b0b4357f5aa9acaac5bcd97548f
Aidan MacNevin <ahmacnevin@wpi.edu>
2023-11-07 11:08:57 -0500

    Added vert swipe animation and identified R vs L

--- frontend/SwipeableCard.js
+++ frontend/SwipeableCard.js
@@ -12,16 +12,38 @@ const SwipeableCard = ({ item,onSwipe,style}) => {
       { useNativeDriver: false }
     ),
     onPanResponderRelease: (e, { dx, dy }) => {
-      const isSwipeOffScreen = Math.abs(dx) > width * 0.5; // consider a swipe if it's more than half the width of the screen
-      if (isSwipeOffScreen) {
+      const isSwipeOffScreen = Math.abs(dx) > width * 0.25; // consider a swipe if it's more than half the width of the screen
+      const isVertSwipe = dy < -height * 0.2;
+      //Negative dx = Left swipe, Positive dx = Right swipe
+      //negative dy = up swipe
+      
+      if (isSwipeOffScreen || isVertSwipe) {
         // Continue the animation off the screen
+        // Determine type of swipe and therefore type of animation 
+        var toVal = { x: dx > 0 ? width : -width, y: dy };
+        if(isVertSwipe) toVal = { x: dx, y: dy < 0 ? -height : dy};
+        
         Animated.timing(pan, {
-          toValue: { x: dx > 0 ? width : -width, y: dy },
+          toValue: toVal,
           duration: 200,
           useNativeDriver: true,
         }).start(() => {
+          if(!isVertSwipe){
+          if(dx > 0){
+            //Right swipe
+            //TODO: HANDLE ADDING TO WATCHLIST 
+            console.log('Right swipe: Handle adding to watchlist');
+          } else{
+            console.log('Left swipe');
+          }
+
           onSwipe(); 
           // Call function to go to next card or handle the swipe off logic
+          } else{
+            // Handle vertical swipe
+            console.log('Handle vertical swipe');
+            onSwipe();
+          }
         });
       } else {
         // Snap back if not swiped off
